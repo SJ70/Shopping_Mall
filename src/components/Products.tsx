a4a5a5c90fd422d76e82b4cbe3efa00755bf9f6f
SJ70 <seojae70@gmail.com>
2024-03-18 14:50:48 +0900

    design: 상품 목록 디자인

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Category, IProduct, categoriesTitle, getProductsByCategory } from '../fakeStoreApi/fakeStoreApi';
 import { Link, useParams } from 'react-router-dom';
+import '../stylesheets/Products.css';
 
 const Products = () => {
 
@@ -18,23 +19,27 @@ const Products = () => {
 
     return (
         <div>
+            <p className='title'>Products</p>
             <ul className='categories'>
                 {Object.entries(categoriesTitle).map(([category, title], idx) => (
                     <li className={(category === categoryId ? 'selected ' : '') + 'category'} key={category}>
-                        <Link to={'../products/'+ category}>
+                        <Link to={'../products/' + category}>
                             {title}
                         </Link>
                     </li>
                 ))}
             </ul>
+            <p className='products-count'>Showing: {products.length} items</p>
             <ul className='products'>
-                <p>Showing: {products.length} items</p>
                 {products.map((product, idx) => (
-                    <li key={product.id}>
-                        <img alt={product.title} src={product.image} width='30px'></img>
-                        <p>{product.title}</p>
-                        <p>$ {product.price}</p>
-                    </li>
+                    <Link to={'../product/' + product.id} className='product' key={product.id}>
+                        <img alt={product.title} src={product.image}></img>
+                        <p className='name'>{product.title}</p>
+                        <div className='product-inner-div'>
+                            <button className='cart'>장바구니에 담기</button>
+                            <p className='price'>$ {product.price}</p>
+                        </div>
+                    </Link>
                 ))}
             </ul>
         </div>
