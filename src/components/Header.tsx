65a737915a1edd733e49a4c63791192c08b194e1
SJ70 <seojae70@gmail.com>
2024-03-19 14:33:47 +0900

    feat: 로그아웃 구현
    - 로그인되어 있을 경우, 로그아웃 버튼 활성화
    
    - 로그인이 되어 있지 않을 경우, 로그인 버튼 활성화

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
 import { Link } from 'react-router-dom';
 import '../stylesheets/Header.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { Action } from 'redux';
+import { RootState } from '../modules';
+import { logout } from '../modules/user';
 
 const Header = () => {
     return (
@@ -8,10 +12,36 @@ const Header = () => {
                 <Link className='logo' to='/products/all'>Shop</Link>
                 <Link className='link' to='/cart'>장바구니</Link>
                 <Link className='link' to='/my-page'>마이페이지</Link>
-                <Link className='link' to='/sign-in'>로그인</Link>
-                <Link className='link' to='/sign-up'>회원가입</Link>
+                <LoginOrLogOut></LoginOrLogOut>
             </div>
         </header>
+    )
+}
+
+const LoginOrLogOut = () => {
+
+    const userId = useSelector((state: RootState) => state.user.uid);
+
+    return userId? (
+        <LogOut></LogOut>
+    ) : (
+        <Link className='link' to='/sign-in'>로그인</Link>
+    )
+}
+
+const LogOut = () => {
+    
+    const dispatch = useDispatch();
+
+    const onLogout = () => dispatch(logout() as Action);
+
+    const onDoLogout = () => {
+        onLogout();
+        window.alert('로그아웃에 성공하였습니다.');
+    }
+
+    return (
+        <button className='logout link' onClick={onDoLogout}>로그아웃</button>
     )
 }
 
--- src/modules/user.ts
+++ src/modules/user.ts
@@ -1,8 +1,10 @@
 import { createAction, handleActions } from 'redux-actions';
 
 const LOGIN = 'user/login';
+const LOGOUT = 'user/logout';
 
 export const login = createAction(LOGIN);
+export const logout = createAction(LOGOUT);
 
 export interface IUser {
     uid: string | null;
@@ -19,6 +21,10 @@ const userReducer = handleActions<IUser, any> (
         [LOGIN]: (state, {payload: {user, email}}) => ({
             uid: user.uid,
             email: user.email,
+        }),
+        [LOGOUT]: () => ({
+            uid: null,
+            email: null,
         })
     },
     initialState
